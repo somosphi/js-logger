44dcdfaba4384c6c3875b5d34d684b57b99259da
Ariel Ril <arielril@gmail.com>
2020-03-12 09:17:16 -0300

    change package files info
    change tsconfig to ignore test files
    add msg to log.info from express package

--- src/packages/ExpressLogger.ts
+++ src/packages/ExpressLogger.ts
@@ -70,12 +70,18 @@ export class ExpressLogger implements IExpressLogger {
         body = rawBody;
       }
 
+      const msg = R.ifElse(
+        R.hasPath(['body', 'message']),
+        R.path(['body', 'message']),
+        R.prop('statusMessage'),
+      )(res);
+
       localLogger.debug({
         requestId,
         headers,
         body,
         type: 'Response',
-      });
+      }, msg);
 
       localLogger.info({
         requestId,
@@ -83,7 +89,8 @@ export class ExpressLogger implements IExpressLogger {
         path: R.prop('url', req),
         'X-Forwarded-For': R.prop('X-Forwarded-For', headers),
         latency: R.prop('X-Request-Time', headers),
-      });
+        statusCode: R.prop('statusCode', res),
+      }, msg);
     };
 
     next();
