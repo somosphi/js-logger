4c831b750830b74beb2421353dc7d37b535c7a8c
Henrique Schmidt <henrique.rieger@gmail.com>
2020-04-07 08:05:08 -0300

    add whitelist

--- src/packages/__tests__/Redact.unit.test.ts
+++ src/packages/__tests__/Redact.unit.test.ts
@@ -1,35 +1,69 @@
 import Redact from '../Redact';
 
 describe('Redact', () => {
-  const redact = new Redact();
-
   test('redact.key true', async () => {
+    const redact = new Redact();
     expect(redact.key('pass')).toBeTruthy();
     expect(redact.key('password')).toBeTruthy();
     expect(redact.key('api-key')).toBeTruthy();
     expect(redact.key('api.key')).toBeTruthy();
     expect(redact.key('api_key')).toBeTruthy();
     expect(redact.key('apikey')).toBeTruthy();
+    expect(redact.key('adminKey')).toBeTruthy();
+    expect(redact.key('authorization')).toBeTruthy();
   });
 
   test('redact.key false', async () => {
+    const redact = new Redact();
     expect(redact.key('passport')).toBeFalsy();
     expect(redact.key('copenhagen')).toBeFalsy();
     expect(redact.key('api*key')).toBeFalsy();
   });
 
+  test('redact.key false', async () => {
+    const redact = new Redact();
+    expect(redact.key('passport')).toBeFalsy();
+    expect(redact.key('copenhagen')).toBeFalsy();
+    expect(redact.key('api*key')).toBeFalsy();
+  });
+
+  test('redact.key false', async () => {
+    const redact = new Redact();
+    expect(redact.key('passport')).toBeFalsy();
+    expect(redact.key('copenhagen')).toBeFalsy();
+    expect(redact.key('api*key')).toBeFalsy();
+  });
+
+  test('redact.key whitelist true', async () => {
+    const redact = new Redact();
+    redact.addWhitelist('authorizationInfo');
+
+    expect(redact.key('authorization')).toBeTruthy();
+  });
+
+  test('redact.key whitelist false', async () => {
+    const redact = new Redact();
+    redact.addWhitelist('authorizationInfo');
+
+    expect(redact.key('authorizationInfo')).toBeFalsy();
+  });
+
   test('redact.value credt card number', () => {
+    const redact = new Redact();
     expect(redact.value('1234 1234 1234 1234')).toBeTruthy();
     expect(redact.value('1234-1234-1234-1234')).toBeTruthy();
     expect(redact.value('1234123412341234')).toBeTruthy();
   });
 
   test('redact.value false', () => {
+    const redact = new Redact();
     expect(redact.value('foobar')).toBeFalsy();
   });
 
 
   test('redact.map', async () => {
+    const redact = new Redact();
+
     const input = {
       foo: 'non-secret',
       secret: 'secret',
